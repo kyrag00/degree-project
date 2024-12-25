80b18c582c1e2ec85e28ef8edf9b8e85ccd8a526
Kyra Getachew <kyragetachew@gmail.com>
2024-12-26 00:42:49 +0100

    using backend api + resolved cors problem

---
+++ client/src/interfaces/IJournalEntry.ts
@@ -0,0 +1,6 @@
+export interface IJournalEntries {
+  id: string;
+  title: string;
+  content: string;
+  mood: string;
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const journalRoutes = require("./routes/journalRoutes");
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 app.use("/journal", journalRoutes);
